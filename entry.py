7f700bde0186d906d00659382651391c9d4b1b41
nadeemishikawa <nadeemishikawa@gmail.com>
2022-09-16 17:02:39 +0900

    changed threading to concurrent.futures

--- entry.py
+++ entry.py
@@ -1,16 +1,18 @@
-from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 
-#from sound_keyboard.keyboard.keyboard import Keyboard
 from gesture_reciever_test import Keyboard
 from sound_keyboard.face_gesture_detector.face_gesture_detector import FaceGestureDetector
 from sound_keyboard.queue import (
     get_queue
 )
 
+
 def main():
     queue = get_queue()
-    Thread(target = lambda : Keyboard(queue).run()).start()
-    Thread(target = lambda : FaceGestureDetector(queue).run()).start()
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(Keyboard(queue).run())
+        executor.submit(FaceGestureDetector(queue).run())
+
 
 if __name__ == '__main__':
     main()
