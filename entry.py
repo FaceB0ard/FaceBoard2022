e51aeb574eb609c056b55dd9a94ecdb25967d713
@Sirius128 <yossymail@sunny.ocn.ne.jp>
2022-09-16 21:43:22 +0900

    change ThreadPoolExecutor to Thread

--- entry.py
+++ entry.py
@@ -1,4 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 
 from sound_keyboard.keyboard.keyboard import Keyboard
 from sound_keyboard.face_gesture_detector.face_gesture_detector import FaceGestureDetector
@@ -8,9 +9,12 @@ from sound_keyboard.queue import get_queue
 def main():
     queue = get_queue()
 
-    with ThreadPoolExecutor() as executor:
+    Thread(target = lambda : Keyboard(queue).run()).start()
+    Thread(target = lambda : FaceGestureDetector(queue).run()).start()
+
+    """with ThreadPoolExecutor() as executor:
         executor.submit(lambda: FaceGestureDetector(queue).run())
-        executor.submit(Keyboard(queue).run())
+        executor.submit(Keyboard(queue).run())"""
 
 
 if __name__ == '__main__':
